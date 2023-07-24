bcb81fa78f85d9f0fb7a7ace87b172f8f8fb2cf2
Mohammad Asad <94637256+ma-asad@users.noreply.github.com>
2023-07-25 02:05:19 +0400

    Code formatted

--- book/book.js
+++ book/book.js
@@ -1,19 +1,19 @@
 // cart open-close
-let cartLogo = document.querySelector('#cart-logo'); 
+let cartLogo = document.querySelector('#cart-logo');
 let cart = document.querySelector('.cart');
 let cartClose = document.querySelector('#cart-close');
 
-cartLogo.onclick = ()=> {
+cartLogo.onclick = () => {
   cart.classList.add("active");
 }
 
-cartClose.onclick = ()=> {
+cartClose.onclick = () => {
   cart.classList.remove("active");
 }
 
 
 // functioning cart
-if (document.readyStates == 'loading'){
+if (document.readyStates == 'loading') {
   document.addEventListener('DOMContentLoaded', ready);
 } else {
   ready();
@@ -21,55 +21,55 @@ if (document.readyStates == 'loading'){
 
 
 // removing item from cart
-function ready(){
-    var removeCartBtn = document.getElementsByClassName('remove');
-    console.log(removeCartBtn);
-    for (var i = 0; i < removeCartBtn.length; i++){
-        var button = removeCartBtn[i]
-        button.addEventListener('click', removeCartItem);
-    }
+function ready() {
+  var removeCartBtn = document.getElementsByClassName('remove');
+  console.log(removeCartBtn);
+  for (var i = 0; i < removeCartBtn.length; i++) {
+    var button = removeCartBtn[i]
+    button.addEventListener('click', removeCartItem);
+  }
 
-// adding to cart
-var addToCart = document.getElementsByClassName('cart-btn');
-for (var i = 0; i < addToCart.length; i++){
+  // adding to cart
+  var addToCart = document.getElementsByClassName('cart-btn');
+  for (var i = 0; i < addToCart.length; i++) {
     var button = addToCart[i]
     button.addEventListener('click', addToCartClicked);
-}
-// buy btn
-document
-.getElementsByClassName('buy-btn')[0]
-.addEventListener('click', buyBtnClicked);
+  }
+  // buy btn
+  document
+    .getElementsByClassName('buy-btn')[0]
+    .addEventListener('click', buyBtnClicked);
 }
 
 
-function removeCartItem(event){
+function removeCartItem(event) {
   var buttonClicked = event.target;
   buttonClicked.parentElement.remove();
   updatetotal();
 }
 
-function addToCartClicked(event){
-    var button = event.target;
-    var ourShopBooks = button.parentElement;
-    var title = ourShopBooks.getElementsByClassName('book-title')[0].innerText;
-    var price = ourShopBooks.getElementsByClassName('price')[0].innerText;
-    var book = ourShopBooks.getElementsByClassName('book-img')[0].src;
-    addBooksToCart(title, price, book);
-    updatetotal();
+function addToCartClicked(event) {
+  var button = event.target;
+  var ourShopBooks = button.parentElement;
+  var title = ourShopBooks.getElementsByClassName('book-title')[0].innerText;
+  var price = ourShopBooks.getElementsByClassName('price')[0].innerText;
+  var book = ourShopBooks.getElementsByClassName('book-img')[0].src;
+  addBooksToCart(title, price, book);
+  updatetotal();
 }
 
 
 
-function addBooksToCart(title, price, book){
-    var cartBoxBooks = document.createElement('div');
-    cartBoxBooks.classList.add('cart-content');
-    var cartProducts = document.getElementsByClassName('cart-box')[0];
-    var cartProductsTitle = cartProducts.getElementsByClassName('cart-book-title');
+function addBooksToCart(title, price, book) {
+  var cartBoxBooks = document.createElement('div');
+  cartBoxBooks.classList.add('cart-content');
+  var cartProducts = document.getElementsByClassName('cart-box')[0];
+  var cartProductsTitle = cartProducts.getElementsByClassName('cart-book-title');
 
 
 
 
-var contentCartBox = `
+  var contentCartBox = `
 
                      <img src="${book}" class="cart-book-img">
                      <div class="book-info">
@@ -79,36 +79,36 @@ var contentCartBox = `
                      <!--close cart-->
                      <i class="fa-solid fa-trash remove"></i>`;
 
-cartBoxBooks.innerHTML = contentCartBox;
-cartProducts.append(cartBoxBooks);
-cartBoxBooks
-.getElementsByClassName('remove')[0]
-.addEventListener('click', removeCartItem);
+  cartBoxBooks.innerHTML = contentCartBox;
+  cartProducts.append(cartBoxBooks);
+  cartBoxBooks
+    .getElementsByClassName('remove')[0]
+    .addEventListener('click', removeCartItem);
 }
 
-function buyBtnClicked(){
+function buyBtnClicked() {
   alert('Thank You For Shopping With Us!');
-    var cartContent = document.getElementsByClassName('cart-box')[0]
-    while (cartContent.hasChildNodes()){
-        cartContent.removeChild(cartContent.firstChild);
-    }
-    updatetotal();
+  var cartContent = document.getElementsByClassName('cart-box')[0]
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updatetotal();
 
 }
 
 
 
 // to update total
-function updatetotal(){
+function updatetotal() {
   var cartContent = document.getElementsByClassName('cart-box')[0];
   var cartBooks = cartContent.getElementsByClassName('cart-content');
   var total = 0;
-  for (var i = 0; i < cartBooks.length; i++){
+  for (var i = 0; i < cartBooks.length; i++) {
     var cartBox = cartBooks[i];
     var priceElement = cartBox.getElementsByClassName('cart-price')[0];
     var price = parseFloat(priceElement.innerText.replace('$', ''));
-    total = total + price; 
+    total = total + price;
   }
 
-    document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+  document.getElementsByClassName('total-price')[0].innerText = '$' + total;
 }
